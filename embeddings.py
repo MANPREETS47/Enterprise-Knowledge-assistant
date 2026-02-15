5e83dfe6367e70e34fd426b90c24f321ac77835a
manpreet singh <manpreeysinghms1080@gmail.com>
2026-02-15 18:37:27 +0530

    Fix

--- embeddings.py
+++ embeddings.py
@@ -1,7 +1,14 @@
 from langchain_huggingface.embeddings import HuggingFaceEmbeddings
 
+_embedding_model = None
+
 def get_embedding_model():
-    print("Maintain Silence Let Me Think...")
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
-    )
+    global _embedding_model
+
+    if _embedding_model is None:
+        print("🔥 Loading embedding model ONCE...")
+        _embedding_model = HuggingFaceEmbeddings(
+            model_name="sentence-transformers/all-MiniLM-L6-v2"
+        )
+
+    return _embedding_model
